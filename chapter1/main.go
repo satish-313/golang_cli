183b3c380a9c7e93186f3245ad5822c5314cb907
satish-313 <pradhansatish53@gmail.com>
2024-09-29 22:04:33 +0530

    first commmit and first chapter

---
+++ chapter1/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	fmt.Println(count(os.Stdin))
+}
+
+func count(r io.Reader) int {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanWords)
+
+	wc := 0
+
+	for scanner.Scan() {
+		wc++
+	}
+
+	return wc
+}
+
+// to run = echo "My first command line program" | ./chapter1
